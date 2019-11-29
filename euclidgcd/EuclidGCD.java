a0b63bd3bdfcf46ff1639f29dd1645d88f6e4245
konekato <18ec033@ms.dendai.ac.jp>
2019-11-29 19:03:37 +0900

    first commit

---
+++ euclidgcd/EuclidGCD.java
@@ -0,0 +1,30 @@
+// ユークリッドの互除法によって最大公約数を求める
+
+import java.util.Scanner;
+
+class EuclidGCD {
+
+	//--- 整数値x,yの最大公約数を求めて返却 ---//
+	static int gcd(int x, int y) {
+		if (y == 0)
+			return x;
+		else
+			return gcd(y, x % y);
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.println("二つの整数の最大公約数を求める。");
+
+		System.out.print("整数値を入力せよ：");
+		int x = stdIn.nextInt();
+
+		System.out.print("整数値を入力せよ：");
+		int y = stdIn.nextInt();
+
+		System.out.println("最大公約数は" + gcd(x, y));
+	}
+
+}
